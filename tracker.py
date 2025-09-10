d2dc7a98ea92af860ae6698e73d8b1e854ed5d2f
SAHFEERUL WASIHF <97935816+SAHFEERULWASIHF@users.noreply.github.com>
2025-09-10 18:59:16 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -42,25 +42,16 @@ def send_email():
 
 url = r"https://www.amazon.in/Apple-MacBook-13-inch-10-core-Unified/dp/B0DZF1485D/ref=sr_1_13?crid=D6ZBLXYF6MZR&dib=eyJ2IjoiMSJ9.2IFLHkotYlL4BG4BUZf33JVCUcDkw7lossku0F_J22DDcAdtXjSR0VUbIx2_ccCjR56phumSBAYkL8oL3O7e8ppnX6aqBLfnFUzBSNa01vIGMqCzS2yL1V5RsxsZu4w5YW0tmcFUzMJY5g_WUdyiGlQNEbF1vu0q-0H6BuOJrZ3hlyjvFrrKy6FiIJdOfgQzjxyQg_gzpY9OZzSmcve1816GL796U_364LyroXlwaa4.UUAZ54Ypp4CBSoyu277X9WTQfhdVINFCwpnkyErOp4s&dib_tag=se&keywords=mac%2Bbook&qid=1757481962&sprefix=mac%2Bbo%2Caps%2C713&sr=8-13&th=1"
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/138.0.7204.185 Safari/537.36"
-    ),
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.185 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
 }
 
 page = requests.get(url, headers = HEADERS)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# name_soup = soup.find(id = 'productTitle')
-
-name = 'Apple 2025 MacBook Air (13-inch, Apple M4 chip with 10-core CPU and 10-core GPU, 24GB Unified Memory, 512GB) - Sky Blue'
+name_soup = soup.find(id = 'productTitle')
+name = name_soup.text.strip()
 
 price_soup = soup.select_one("span.a-price span.a-offscreen")
 price = int(float(price_soup.text.replace(",","").replace("₹","")))
@@ -74,4 +65,5 @@ new_row = {'Name': name, 'Price': price, 'Date': day}
 
 df = pd.concat([df,pd.DataFrame([new_row])], ignore_index=True)
 print(df)
+
 
